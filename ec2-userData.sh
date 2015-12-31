887f2740a552a72a52e244db434ef3bcec598cd9
behrica <carsten.behring@gmail.com>
2015-12-31 18:52:00 +0100

    Removed -vv

--- ec2-userData.sh
+++ ec2-userData.sh
@@ -7,4 +7,4 @@ apt-get -y install python-pip
 apt-get -y install git-core
 pip install boto
 printf '[tag_ds_Yes]\nlocalhost\n' > /tmp/hosts
-ansible-pull  -vv -i /tmp/hosts -U https://github.com/behrica/provisionDataScience > /tmp/ansible-pull.log
+ansible-pull  -i /tmp/hosts -U https://github.com/behrica/provisionDataScience > /tmp/ansible-pull.log
